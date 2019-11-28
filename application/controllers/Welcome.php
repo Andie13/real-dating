18d13d2f1ea210c01d81a348f6dd7ca631b86fa4
Andie13 <fody.fady@gmail.com>
2019-11-28 17:36:45 +0100

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -40,7 +40,7 @@ class Welcome extends CI_Controller {
         $this->load->view('layout/footer');
         
     }
-	public function gotoLegel() {
+	public function gotoLegal() {
         
         $this->load->view('layout/header');
         $this->load->view('legal_view');
