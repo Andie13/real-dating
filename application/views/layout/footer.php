4d98e66cb1fc057e2cbe23ea5a36202fdb455235
Andie13 <fody.fady@gmail.com>
2019-11-28 18:28:28 +0100

    added footer link

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,16 +1,17 @@
 <div class="row">
     <footer class="footer-a col-lg-12 ">
-        <div class="col-4">   
+        <div class="col-3">   
             <ul>        
                 <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoLegal">Mentions legales</a></p></li>
+				<li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoCGU">CGU</a></p></li>
             </ul>
         </div>
-        <div class=" col-4">   
+        <div class=" col-3">   
             <ul>        
                 <li class="menu"><p><a href="<?php echo base_url() ?>user/inscription_controller"> copyright </a></p></li>                       
             </ul>
         </div>
-        <div class="col-4">   
+        <div class="col-3">   
             <ul>        
                 <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">À propos</a></p></li>
                 <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">Nous Contacter</a></p></li>
