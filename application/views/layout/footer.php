2842dc94a29864d977ca36815d2a22f85dbdf6bb
Andie13 <fody.fady@gmail.com>
2019-11-29 15:47:12 +0100

    footer disposal

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,21 +1,20 @@
 <div class="row">
     <footer class="footer-a col-lg-12 ">
-	    <div class="col-12">
-        <div class="col-3">   
+	    <div class="col-12 top-footer">
+        <div class="col-4">   
             <ul>        
-                <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoLegal">Mentions legales</a></p></li>
 				<li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoCGU">CGU</a></p></li>
             </ul>
         </div>
-        <div class=" col-3">   
+        <div class=" col-4">   
             <ul>        
-                <li class="menu"><p><a href="<?php echo base_url() ?>user/inscription_controller"> copyright </a></p></li>                       
+            <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">Nous Contacter</a></p></li>                   
             </ul>
         </div>
-        <div class="col-3">   
+        <div class="col-4">   
             <ul>        
                 <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">À propos</a></p></li>
-                <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">Nous Contacter</a></p></li>
+              
 
             </ul>
         </div>
