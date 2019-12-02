039c87566fab27dc507e70908f78f34714664892
Andie13 <fody.fady@gmail.com>
2019-12-02 11:06:49 +0100

    retrait logo twitter
    logo twitter
    lien mention légales

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -6,23 +6,23 @@
 				<li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoCGU">CGU</a></p></li>
             </ul>
         </div>
-        <div class=" col-4">   
-            <ul>        
-            <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">Nous Contacter</a></p></li>                   
-            </ul>
-        </div>
+       
         <div class="col-4">   
             <ul>        
-                <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">À propos</a></p></li>
+                <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoLegal">Mentions Légales</a></p></li>
               
 
+            </ul>
+        </div>
+	 <div class=" col-4">   
+            <ul>        
+            <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">Nous Contacter</a></p></li>                   
             </ul>
         </div>
 	    <div class="col-12 ">
 		    <div class="footer-medias">
 			    <a href="https://www.facebook.com/RealDate.fr/"> <img src="<?php echo base_url() ?>assets/images/rd-fb.png" alt="logo instagram"/> </a>
 		     <a href="">  <img src="<?php echo base_url() ?>assets/images/rd-insta.png" alt="logo instagram"/></a>
-		      <a href=""> <img src="<?php echo base_url() ?>assets/images/rd-twitter.png" alt="logo facebook"/></a>
 		  
 		    </div>
 		    
