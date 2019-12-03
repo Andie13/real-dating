ab30a1db2e3482d115a490193ae13d5a8e89b82c
Andie13 <fody.fady@gmail.com>
2019-12-03 12:12:05 +0100

    added div

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,32 +1,35 @@
 <div class="row">
     <footer class="footer-a col-lg-12 ">
-	    <div class="col-12 top-footer">
-        <div class="col-4">   
-            <ul>        
-				<li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoCGU">CGU</a></p></li>
-            </ul>
-        </div>
-       
-        <div class="col-4">   
-            <ul>        
-                <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoLegal">Mentions Légales</a></p></li>
-              
+        <div class="col-12 top-footer">
+            <div class="col-4">   
+                <ul>        
+                    <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoCGU">CGU</a></p></li>
+                </ul>
+            </div>
 
-            </ul>
-        </div>
-	 <div class=" col-4">   
-            <ul>        
-            <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">Nous Contacter</a></p></li>                   
-            </ul>
-        </div>
-	    <div class="col-12 ">
-		    <div class="footer-medias">
-			    <a href="https://www.facebook.com/RealDate.fr/"> <img src="<?php echo base_url() ?>assets/images/rd-fb.png" alt="logo instagram"/> </a>
-		     <a href="https://www.instagram.com/real_date13100/">  <img src="<?php echo base_url() ?>assets/images/rd-insta.png" alt="logo instagram"/></a>
-		  
-		    </div>
-		    
-		     </div>
+            <div class="col-4">   
+                <ul>        
+                    <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoLegal">Mentions Légales</a></p></li>
+
+
+                </ul>
+            </div>
+            <div class=" col-4">   
+                <ul>        
+                    <li class="menu"><p><a href="<?php echo base_url() ?>welcome/gotoAbout">Nous Contacter</a></p></li>                   
+                </ul>
+            </div>
+            <div class="col-12 ">
+                <div class="footer-medias">
+                    <div class="social-link">
+                        <a href="https://www.facebook.com/RealDate.fr/"> <img src="<?php echo base_url() ?>assets/images/rd-fb.png" alt="logo instagram"/> </a>
+                        <a href="https://www.instagram.com/real_date13100/">  <img src="<?php echo base_url() ?>assets/images/rd-insta.png" alt="logo instagram"/></a>
+
+                    </div>
+                   
+                </div>
+
+            </div>
     </footer>
 </div>
 </div>
