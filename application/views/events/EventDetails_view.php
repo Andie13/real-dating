07c4b4877547f2dbf281dced5dd321132f677b9c
Andie13 <fody.fady@gmail.com>
2019-12-03 10:29:34 +0100

    motif

--- application/views/events/EventDetails_view.php
+++ application/views/events/EventDetails_view.php
@@ -12,41 +12,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/loog-carre1000x1000.png" alt="logo"/>
                 </p>
             </div>
-            <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
+           <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
                 <a id="logo-res" >                   
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/logo-favicon-carre-1000.png" alt="logo"/>
                 </a>
-                <a href = "#" class="active">Accueil</a>
-
-
                 <?php
                 if (isset($connected)) {
-
+		    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
                     echo '<a href = "' . base_url() . 'user/UserProfile_controller">Mon Profile</a>';
-                    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
+                   
                 } else {
                     echo '<a href = "' . base_url() . 'user/Login_controller">Connexion</a>';
                     echo '<a href = "' . base_url() . 'user/inscription_controller"> Inscription</a>';
                 }
                 ?>
 
-
+		    <a href = "<?php echo base_url()?>welcome/gotoConcept"">Le concept</a>
+			 <a href = "<?php echo base_url()?>welcome/" class="">Accueil</a>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                     <i class="fa fa-bars"></i>
                 </a>
             </div>
         </div>
-
         <div class=" col-lg-12 col-xs-12 contain-wrap ">
             <div class="row">
                 <div class="col-lg-12">
-
                 </div>
-
             </div>
             <div class="row">
                 <div class="col-lg-10 col-xs-10 event eventDetails">
-                    <div class="col-lg-6 col-xs-10 containerImage" >
+								   <div class="retour"><a href=" <?php echo base_url().'events/Events_controller?ville='. $ville->nom_commune ?>"> <h3> <<  RETOUR VERS LA LISTE D'ÉVÉNEMENTS  <<</h3></a></div>
+                    <div class="col-lg-6 col-xs-10 containerImage" > <div class="col-lg-6 col-xs-10 containerImage" >
                         <?php
                         if ($media != '') {
                             echo '<img id="imageEvent" src="' . $media->path_media . '/' . $media->nom_media . '" alt="image event"/>';
