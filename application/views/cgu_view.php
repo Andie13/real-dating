fcd2004e7fe57c727ffed1a9438be295abb8159f
Andie13 <fody.fady@gmail.com>
2019-12-04 17:48:07 +0100

    nav

--- application/views/cgu_view.php
+++ application/views/cgu_view.php
@@ -12,33 +12,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/loog-carre1000x1000.png" alt="logo"/>
                 </p>
             </div>
-            <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
+           <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
                 <a id="logo-res" >                   
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/logo-favicon-carre-1000.png" alt="logo"/>
                 </a>
-                <a href = "#" class="active">CGU</a>
-
-
                 <?php
                 if (isset($connected)) {
-
+		    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
                     echo '<a href = "' . base_url() . 'user/UserProfile_controller">Mon Profile</a>';
-                    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
+                   
                 } else {
                     echo '<a href = "' . base_url() . 'user/Login_controller">Connexion</a>';
                     echo '<a href = "' . base_url() . 'user/inscription_controller"> Inscription</a>';
                 }
                 ?>
 
-		  
-		      	 <a href = "<?php echo base_url()?>welcome/gotoconcept_view" class="">Le concept</a>
-		       <a href = "<?php echo base_url()?>welcome/" class="">Accueil</a>
-
+		        <a href = "<?php echo base_url()?>welcome/gotoConcept" class="">Le concept</a>
+		 <a href = "<?php echo base_url()?>welcome/" class="">Accueil</a>
+		
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                     <i class="fa fa-bars"></i>
                 </a>
             </div>
         </div>
+
 
 	    <div class="clearfix"></div>
         <div class=" col-lg-12 col-xs-12 content">
