354f0ed392cd5f5f54d13ed6a45d7af8f6889e1a
Andie13 <fody.fady@gmail.com>
2019-12-04 17:34:46 +0100

    nav inscription

--- application/views/user/Inscription_view.php
+++ application/views/user/Inscription_view.php
@@ -11,27 +11,27 @@
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/loog-carre1000x1000.png" alt="logo"/>
                 </p>
             </div>
-            <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
+              <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
                 <a id="logo-res" >                   
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/logo-favicon-carre-1000.png" alt="logo"/>
                 </a>
-                <a href = "#" class="active">Inscription</a>
-					<a href = "<?php echo base_url()?>welcome/gotoconcept_view" class="">Le concept</a>
+                
+				
 
 
                 <?php
                 if (isset($connected)) {
-
+		    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
                     echo '<a href = "' . base_url() . 'user/UserProfile_controller">Mon Profile</a>';
-                    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
+                   
                 } else {
-
                     echo '<a href = "' . base_url() . 'user/Login_controller">Connexion</a>';
-                    echo '<a href = "' . base_url() . 'welcome">Accueil</a>';
+                    echo '<a href = "" class="active"> Inscription</a>';
                 }
                 ?>
-					
 
+		    <a href = "<?php echo base_url()?>welcome/gotoConcept">Le concept</a>
+		 <a href = "<?php echo base_url()?>welcome/" class="">Accueil</a>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                     <i class="fa fa-bars"></i>
                 </a>
