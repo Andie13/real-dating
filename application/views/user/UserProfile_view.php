13754f6fcc7db782fe26523d07d51e7318fd7900
Andie13 <fody.fady@gmail.com>
2019-12-07 11:29:35 +0100

    ortho nav

--- application/views/user/UserProfile_view.php
+++ application/views/user/UserProfile_view.php
@@ -8,30 +8,32 @@
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/loog-carre1000x1000.png" alt="logo"/>
                 </p>
             </div>
-            <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
+          <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
                 <a id="logo-res" >                   
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/logo-favicon-carre-1000.png" alt="logo"/>
                 </a>
-                <a href class="active">Mon Profile</a>
+                
+				
 
 
                 <?php
                 if (isset($connected)) {
-
-
-                    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
-                    echo '<a href = "' . base_url() . 'welcome">Accueil</a>';
+		    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
+                    echo '<a class="active" href = "' . base_url() . 'user/UserProfile_controller">Mon Profil</a>';
+                   
                 } else {
                     echo '<a href = "' . base_url() . 'user/Login_controller">Connexion</a>';
-                    echo '<a href = "' . base_url() . 'user/inscription_controller"> Inscription</a>';
+                    echo '<a href ="' . base_url() . 'user/inscription_controller" > Inscription</a>';
                 }
                 ?>
 
-
+		    <a href = "<?php echo base_url()?>welcome/gotoConcept">Le concept</a>
+		 <a href = "<?php echo base_url()?>welcome/" class="">Accueil</a>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                     <i class="fa fa-bars"></i>
                 </a>
             </div>
+        </div>
         </div>
         <div class="clearfix"></div>
 
