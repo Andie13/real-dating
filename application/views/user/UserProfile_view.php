30bb6d0cd613e03d4e354bbf705711b471a1ec37
Andie13 <fody.fady@gmail.com>
2019-12-13 11:50:37 +0100

    Update UserProfile_view.php

--- application/views/user/UserProfile_view.php
+++ application/views/user/UserProfile_view.php
@@ -1,60 +1,62 @@
 <body class=" bg welcome">
-	<div class="container-fluid login-wrapper">
-		<div class="header col-lg-12 col-xs-12 ">
+    <div class="container-fluid login-wrapper">
+        <div class="header col-lg-12 col-xs-12 ">
             -     <div class="col-lg-4 col-xs-12 logo">
                 <p id="plogo">
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/loog-carre1000x1000.png" alt="logo"/>
                 </p>
             </div>
-          <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
+            <div class="topnav col-lg-8 col-xs-12" id="myTopnav">
                 <a id="logo-res" >                   
                     <img id="headerImg" src="<?php echo base_url(); ?>assets/images/logo/logo-favicon-carre-1000.png" alt="logo"/>
                 </a>
 
                 <?php
                 if (isset($connected)) {
-		    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
+                    echo '<a href = "' . base_url() . 'user/login_controller/logout">Déconnexion</a>';
                     echo '<a class="active" href = "' . base_url() . 'user/UserProfile_controller">Mon Profil</a>';
-                   
                 } else {
                     echo '<a href = "' . base_url() . 'user/Login_controller">Connexion</a>';
                     echo '<a href ="' . base_url() . 'user/inscription_controller" > Inscription</a>';
                 }
                 ?>
 
-		    <a href = "<?php echo base_url()?>welcome/gotoConcept">Le concept</a>
-		 <a href = "<?php echo base_url()?>welcome/" class="">Accueil</a>
+                <a href = "<?php echo base_url() ?>welcome/gotoConcept">Le concept</a>
+                <a href = "<?php echo base_url() ?>welcome/" class="">Accueil</a>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                     <i class="fa fa-bars"></i>
                 </a>
             </div>
         </div>
-		 <div class="clearfix"></div>
-			<div class="content">
-				 <div class="row">
-            <div class=" col-12 accroche">
-                <p><i>"Gestion de votre profile:"</i> </p>
-                <p><i>Bonjour <?php echo $user->nom_user . ' ' . $user->prenom_user ?></i> </p>
-                <p><i></i></p>
+        <div class="clearfix"></div>
+        <div class="content">
+            <div class="row">
+                <div class=" col-12 accroche">
+                    <p><i>"Gestion de votre profile:"</i> </p>
+                    <p><i>Bonjour <?php echo $user->nom_user . ' ' . $user->prenom_user ?></i> </p>
+                    <p><i></i></p>
+                </div>
             </div>
+            <div id="tabs" class="col-lg-10 col-xs-12 eventDetails   ">
+                <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                    <ul class="nav nav-tabs  ">
+                        <li class="active">
+                            <a href="#id" data-toggle="tab">
+                                <i class="fa fa-user"></i>Identification
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#resas" data-toggle="tab">
+                                <i class="fa fa-calendar"> Mes Soirées</i>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+		    <div class="tab-content ">
+			    
+		    </div>
+            </div>
+
         </div>
-				 <div id="tabs" class="col-lg-10 col-xs-12 eventDetails   ">
-					  <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
-                <ul class="nav nav-tabs  ">
-                    <li class="active">
-                        <a href="#id" data-toggle="tab">
-                            <i class="fa fa-user"></i>Identification
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#resas" data-toggle="tab">
-                            <i class="fa fa-calendar"> Mes Soirées</i>
-                        </a>
-                    </li>
 
-                </ul>
-            </div>
-					 </div>
-
-		</div>
-		
