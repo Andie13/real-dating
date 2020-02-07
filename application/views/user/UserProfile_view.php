60fb9527613886c85bec4926ea9f8de601fc13b6
Andie13 <fody.fady@gmail.com>
2020-02-07 14:36:32 +0100

    bug menu

--- application/views/user/UserProfile_view.php
+++ application/views/user/UserProfile_view.php
@@ -261,4 +261,14 @@
 		   
             });
         </script>
+	    <script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
 
