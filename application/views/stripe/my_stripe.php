d7469845eb39a52bd44383541067b64169a38122
Andie13 <fody.fady@gmail.com>
2020-02-07 17:03:08 +0100

    add js

--- application/views/stripe/my_stripe.php
+++ application/views/stripe/my_stripe.php
@@ -179,3 +179,13 @@ $(function() {
      
 });
 </script>
+		<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
