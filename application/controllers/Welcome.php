80b56a3b12d98fcf7fe031b73c191b2a67e9a6f9
Andie13 <fody.fady@gmail.com>
2020-02-14 15:30:49 +0100

    update session visibility

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -33,51 +33,52 @@ class Welcome extends CI_Controller {
         $villesModel->getVilles('Marseille');
     }
 
-    public function gotoAbout() {
-
+    public function gotoLegal() {
         $this->load->view('layout/header');
-        $this->load->view('about_view');
+        if ($this->session->connected) {
+
+            $data = array(
+                'userId' => $this->session->userId,
+                'connected' => $this->session->connected,
+            );
+
+            $this->load->view('legal_view', $data);
+        } else {
+            $this->load->view('legal_view');
+        }
         $this->load->view('layout/footer');
     }
 
-    public function gotoLegal() {
-
-        if ($this->session->connected) {
-
-            $data = array(
-                'userId' => $this->session->userId,
-                'connected' => $this->session->connected,
-            );
-            $this->load->view('layout/header');
-            $this->load->view('legal_view', $data);
-            $this->load->view('layout/footer');
-        }
-    }
-
     public function gotoCGU() {
+        $this->load->view('layout/header');
         if ($this->session->connected) {
 
             $data = array(
                 'userId' => $this->session->userId,
                 'connected' => $this->session->connected,
             );
-            $this->load->view('layout/header');
+
             $this->load->view('cgu_view', $data);
-            $this->load->view('layout/footer');
+        } else {
+            $this->load->view('cgu_view');
         }
+        $this->load->view('layout/footer');
     }
 
     public function gotoConcept() {
+        $this->load->view('layout/header');
         if ($this->session->connected) {
 
             $data = array(
                 'userId' => $this->session->userId,
                 'connected' => $this->session->connected,
             );
-            $this->load->view('layout/header');
+
             $this->load->view('concept_view', $data);
-            $this->load->view('layout/footer');
-        }
+        } else {
+
+            $this->load->view('concept_view');
+        } $this->load->view('layout/footer');
     }
+
 }
-    
