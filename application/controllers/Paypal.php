9d98f5b7e0268ae6f9c5bd4dd524a499122ef09d
Andie13 <fody.fady@gmail.com>
2020-03-13 15:26:34 +0100

    change func declaration

--- application/controllers/Paypal.php
+++ application/controllers/Paypal.php
@@ -15,13 +15,16 @@ class Paypal extends CI_Controller{
         if(isset($this->session->id_event)){
            
             
+            $EventModel = new Events_model();
+
+
             $idEvent = $this->session->id_event;
             $idUser = $this->session->userId;
-         
-            $EventModel = new Events_model();
-            $EventModel->insertNewReservation($idUser, $idEvent);
-            
-            $this->session->unset_userdata['id_event'];
+            $ev = $EventModel->getEventDetailsById($idEvent);
+            $prix = $ev->prix_event;
+
+            $EventModel->insertNewReservation($idUser, $idEvent, 2, 1, $prix);
+
             
         }
         // Get the transaction data
