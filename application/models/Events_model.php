f3e9b0cb78abd9a65e791e15d97dfc5996ae3819
Andie13 <fody.fady@gmail.com>
2020-03-13 13:03:47 +0100

    update insert new resa

--- application/models/Events_model.php
+++ application/models/Events_model.php
@@ -23,6 +23,9 @@ class Events_model extends CI_Model {
     const ID_PRESTA_EVENT = 'id_presta_event';
     const PRIX_EVENT = 'prix_event';
     const ID_IMAGE_EVENT = 'id_image_event';
+    const ID_TYPE_PAYMT = 'id_type_paymt';
+    const ID_PROMO = 'id_promo';
+    const PRIX_RESA = 'prix';
 
     public function getEvents($idVille) {
         $this->db->where(self::ID_VILLE, $idVille)
@@ -48,7 +51,7 @@ class Events_model extends CI_Model {
         return $insert ? true : false;
     }
 
-    public function insertNewReservation($idUser, $idEvent) {
+    public function insertNewReservation($idUser, $idEvent, $idTypePay, $idPromo, $prix) {
 
         $date = date('Y-m-d H:i:s');
         $ref = 'RD-' . $date;
@@ -58,7 +61,10 @@ class Events_model extends CI_Model {
             self::ID_EVENT => $idEvent,
             self::DATE_RESA => $date,
             self::REF_RESA => $ref,
-			self::STATUS_RESA =>2
+            self::STATUS_RESA =>2,
+            self::ID_TYPE_PAYMT =>$idTypePay,
+            self::ID_PROMO => $idPromo,
+            self::PRIX_RESA => $prix
         ];
 
         $this->db->insert(self::TABLE_RESA, $data);
@@ -68,6 +74,7 @@ class Events_model extends CI_Model {
             false;
         }
     }
+
 
     public function getEventByUserId($id) {
 
